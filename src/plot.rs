dfd2230479afe970894e45168e300d6c9a9679dd
dan5 <dngzz1@gmail.com>
2022-10-26 22:13:10 +0800

    can plot cluster

--- src/plot.rs
+++ src/plot.rs
@@ -1,3 +1,4 @@
+use crate::anneal;
 use crate::cluster;
 use plotters::prelude::*;
 pub fn plot_hamiltonian_loop(_points: &[(f32, f32)]) {
@@ -36,7 +37,7 @@ pub fn plot_hamiltonian_loop(_points: &[(f32, f32)]) {
     .unwrap();
 }
 
-pub fn plot_clusters(_points: &[(f32, f32)], _salesmen_capacities: &[usize]) {
+pub fn plot_clusters(_points: &[(f32, f32)], _salesmen_capacities: &[usize], filename: &str) {
     // generate loop from points.
     let points = _points.to_vec();
     let salesmen_capacities = _salesmen_capacities.to_vec();
@@ -45,7 +46,6 @@ pub fn plot_clusters(_points: &[(f32, f32)], _salesmen_capacities: &[usize]) {
     // Plot the result and save to folder images/<filename>
     let my_colors = [&BLACK, &RED, &BLUE, &GREEN, &MAGENTA, &CYAN];
 
-    let filename = "clustering";
     let pathname = &format!("images/{}.png", filename);
     let root_drawing_area = BitMapBackend::new(pathname, (768, 768)).into_drawing_area();
     root_drawing_area.fill(&WHITE).unwrap();
@@ -59,10 +59,22 @@ pub fn plot_clusters(_points: &[(f32, f32)], _salesmen_capacities: &[usize]) {
         let range_start = salesmen_capacities[0..i].iter().sum::<usize>();
         let range_end = salesmen_capacities[0..(i + 1)].iter().sum::<usize>();
         let range = range_start..range_end;
+        let filtered_points = sorted_points[range].iter().map(|&x| x).collect::<Vec<_>>();
+        let my_color = my_colors[i % my_colors.len()];
+        // run salesman algorithm on filtered_points.
+        let mut shortest_path = anneal::shortest_path(&filtered_points, 1);
+        shortest_path.push(shortest_path[0]);
         ctx.draw_series(
-            sorted_points[range]
+            filtered_points
                 .iter()
-                .map(|point| TriangleMarker::new(*point, 5, my_colors[i % my_colors.len()])),
+                .map(|point| TriangleMarker::new(*point, 5, my_color)),
+        )
+        .unwrap();
+
+        ctx.draw_series(
+            vec![1]
+                .iter()
+                .map(|_| PathElement::new(shortest_path.clone(), my_color)),
         )
         .unwrap();
     }
