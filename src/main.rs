66178d8e7831851075d7944aa8042a466d049b99
dan5 <dngzz1@gmail.com>
2022-10-26 16:39:13 +0800

    created plot.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use plotters::prelude::*;
 pub mod example;
+pub mod plot;
 use example::*;
 use salesman::anneal::*;
 
@@ -14,36 +14,7 @@ fn main() {
         }
         rand_points = temp_points;
     }
+
     let points = shortest_path(&rand_points, 1);
-    let salesman_loop;
-    {
-        let mut temp = points.clone();
-        temp.push(temp[0]);
-        salesman_loop = temp;
-    }
-
-    // Plot the result and save to folder images/<filename>
-    let filename = "salesman";
-    let pathname = &format!("images/{}.png", filename);
-    let root_drawing_area = BitMapBackend::new(pathname, (768, 768)).into_drawing_area();
-    root_drawing_area.fill(&WHITE).unwrap();
-
-    let mut ctx = ChartBuilder::on(&root_drawing_area)
-        .caption("Travelling Salesman", ("sans-serif", 40))
-        .build_cartesian_2d(-1.2_f32..1.2, -1.2_f32..1.2)
-        .unwrap();
-
-    ctx.draw_series(
-        points
-            .iter()
-            .map(|point| TriangleMarker::new(*point, 5, &BLACK)),
-    )
-    .unwrap();
-
-    ctx.draw_series(
-        vec![1]
-            .iter()
-            .map(|_| PathElement::new(salesman_loop.clone(), &BLUE)),
-    )
-    .unwrap();
+    plot::plot(&points);
 }
---
+++ src/plot.rs
@@ -0,0 +1,37 @@
+use plotters::prelude::*;
+
+pub fn plot(_points: &[(f32, f32)]) {
+    // generate loop from points.
+    let points = _points.iter().map(|&x| x).collect::<Vec<_>>();
+    let salesman_loop;
+    {
+        let mut temp = points.clone();
+        temp.push(temp[0]);
+        salesman_loop = temp;
+    }
+
+    // Plot the result and save to folder images/<filename>
+    let filename = "salesman";
+    let pathname = &format!("images/{}.png", filename);
+    let root_drawing_area = BitMapBackend::new(pathname, (768, 768)).into_drawing_area();
+    root_drawing_area.fill(&WHITE).unwrap();
+
+    let mut ctx = ChartBuilder::on(&root_drawing_area)
+        .caption("Travelling Salesman", ("sans-serif", 40))
+        .build_cartesian_2d(-1.2_f32..1.2, -1.2_f32..1.2)
+        .unwrap();
+
+    ctx.draw_series(
+        points
+            .iter()
+            .map(|point| TriangleMarker::new(*point, 5, &BLACK)),
+    )
+    .unwrap();
+
+    ctx.draw_series(
+        vec![1]
+            .iter()
+            .map(|_| PathElement::new(salesman_loop.clone(), &BLUE)),
+    )
+    .unwrap();
+}
